a80b635783a6b53d0c558311f52fa9de79597614
fwelland <fwelland@weedeater.promontory-hq.promcapital.com>
2015-12-29 15:44:11 -0500

    misc updates

--- src/main/java/fhw/GetFormController.java
+++ src/main/java/fhw/GetFormController.java
@@ -1,17 +1,27 @@
 package fhw;
 
+import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
+import javax.validation.constraints.Max;
 import lombok.Data;
 
 @Named
 @Data
+@RequestScoped
 public class GetFormController
 {
-    private String valueFromGet; 
-    private Integer formIValue; 
-    
+    private String valueFromGet;
+
+    @Max(50)
+    private Integer formIValue;
+
     public GetFormController()
     {
-        
+
+    }
+    
+    public void setValueFromGet(String s)
+    {
+        valueFromGet = s;
     }
 }
