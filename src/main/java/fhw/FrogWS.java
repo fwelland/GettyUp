6596f62db139a2341748a2513321d83d2d421949
fwelland <fwelland@weedeater>
2016-02-03 13:12:07 -0500

    adding in a hacky jaxrs thing

---
+++ src/main/java/fhw/FrogWS.java
@@ -0,0 +1,32 @@
+package fhw;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.UriInfo;
+
+
+@Path("frogws")
+public class FrogWS
+{
+    @Context
+    private UriInfo context;
+
+    public FrogWS()
+    {
+    }
+
+    @GET
+    @Path("{content_id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String acknowledge(@PathParam("content_id")String content_id)
+    {
+        System.out.println("content_id " + content_id);
+        return "{}";
+    }
+}
