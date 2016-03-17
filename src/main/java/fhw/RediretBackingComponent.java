5d96bb5e193be5605e991add4364bebd7c3b44a9
fwelland <fwelland@weedeater>
2016-03-16 21:29:29 -0400

    ajax redirect composite component

--- src/main/java/fhw/RediretBackingComponent.java
+++ src/main/java/fhw/RediretBackingComponent.java
@@ -1,9 +1,10 @@
 package fhw;
 
-import javax.annotation.PostConstruct;
+import java.io.IOException;
 import javax.faces.component.FacesComponent;
 import javax.faces.component.UINamingContainer;
-
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 
 @FacesComponent("redirectBacker")
 public class RediretBackingComponent
@@ -13,10 +14,9 @@ public class RediretBackingComponent
 
     private String linkLable;
 
-    @PostConstruct
-    private void init()
+    public void init()
     {
-        System.out.println("RediretBackingComponent post constructor was called:  " + this);
+        System.out.println("RediretBackingComponent init:  " + this);
         String s = (String)getAttributes().get("title");
         if(null != s)
         {
@@ -25,10 +25,8 @@ public class RediretBackingComponent
         else
         {
             linkLable = "nothing passed to me";
-        }                
+        }
     }
-    
-
     public String getLinkLable()
     {
         System.out.println("RediretBackingComponent::getLinkLable:  " + this);
@@ -36,7 +34,10 @@ public class RediretBackingComponent
     }
 
     public void someAction()
+            throws IOException
     {
         System.out.println("RediretBackingComponent::someAction:  " + this);
+        ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+        ec.redirect(ec.getRequestContextPath()+"/");
     }
 }
